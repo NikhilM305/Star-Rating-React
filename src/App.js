5940d606615a7dc9eb26f993d93de284b3c5612b
NikhilM305 <nikhil.ninja007@gmail.com>
2023-02-23 19:27:47 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,88 @@
+import React, {useState ,useReducer} from 'react';
+import Todo from './Componennts/Todo';
+import {FaStar} from "react-icons/fa"
+import {FaHourglassStart} from "react-icons/fa"
+
+
+
+
+
+const colors = {
+  orange : "#FD4621",
+  grey : "#9D918E"
+
+}
+
+
+
+ function App() {
+const stars = Array(5).fill(0)
+const [currentValue,setCurrentValue] = React.useState(0)
+const [hoverValue , setHoverValue] = React.useState(undefined)
+
+
+const handleClick = value=>{
+  setCurrentValue(value)
+}
+
+const handleMouseOver = value=>{
+  setHoverValue(value)
+}
+
+const handleMouseLeave = () =>{
+  setHoverValue(undefined)
+}
+
+
+
+
+  return (
+
+
+    <div style={styles.container}>
+    <h2>Star Rating in React</h2>
+   <div style={styles.stars}>
+        {
+          stars.map((_,index)=>{
+            return <FaStar key={index} 
+            size={24}
+            style={{
+              cursor:"pointer"
+            }}
+         color={(hoverValue || currentValue)>index ? colors.orange :colors.grey}
+
+            onClick={()=>{
+              handleClick(index+1)
+            }}
+            onMouseOver={() => handleMouseOver(index+1)} 
+            onMouseLeave={handleMouseLeave}
+
+             
+            />
+          })
+        }
+   </div>
+
+    </div>
+
+
+
+
+  );
+}
+
+
+const styles = {
+  
+   container:{
+  
+    display:"flex",
+    flexDirection: "column",
+    alignItems:"center",
+    height:"100vh",
+    justifyContent:"center"
+   }
+
+}
+
+export default App;
